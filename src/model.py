44648f9f7631525635b977110cd1580b679e432b
HugoPhi <hugonelson07@outlook.com>
2024-09-04 15:27:54 +0800

    first commit

--- src/model.py
+++ src/model.py
@@ -53,24 +53,6 @@ class har_LSTMBaseline(torch.nn.Module):
         return out
 
 class har_ResBiLSTM(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(har_ResBiLSTM, self).__init__()
-        self.lstm = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True, bidirectional=True)
-        self.fc = torch.nn.Linear(hidden_size * 2, num_classes)
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.num_classes = num_classes
-
-        
-    def forward(self, x):
-        h0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(x.device)
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])  # 使用最后一个时间步的输出
-        return out
-
-class ResBiLSTM(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, dropout_rate, lambda_l2):
         super(ResBiLSTM, self).__init__()
         
